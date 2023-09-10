e4f5f523757e32324fa6827dcb5dec7cf80aaa96
fiokee <smleffiong@gmail.com>
2023-09-10 21:13:20 +0100

    working with express and middleware

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+
+app.use((req, res, next)=>{
+    console.log('next middleware');
+    next(); //this allows request to continue to the next middleware in line
+});
+
+app.use((req, res, next)=>{
+    console.log('another middleware')
+    res.send('<h1>Hello People of Node</h1>')
+})
 const server = http.createServer(app)
 
 server.listen(3000, ()=>{
