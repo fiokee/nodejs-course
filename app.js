211127240371c9511452e5e6cea1946655a993fc
fiokee <smleffiong@gmail.com>
2023-09-12 18:16:24 +0100

    adding body parser

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-
-// const routes = require('./routes')
-
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+//handling different routes
 
-app.use((req, res, next)=>{
-    console.log('next middleware');
-    next(); //this allows request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+// A route returning a form
+app.use('/add-products',(req, res, next)=>{
+    console.log('another middleware');
+    res.send('<body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>');
 });
 
-app.use((req, res, next)=>{
+// redirecting to a different route
+app.use('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/', (req, res, next)=>{
     console.log('another middleware');
     res.send('<h1>Hello People of Node</h1>');
-})
-
-
+});
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
-})
+});
 
 
